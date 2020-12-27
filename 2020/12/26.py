7777f299ff0fb1db50afc7f6d89ad82e78e14613
Frank Piva <piva.post@gmail.com>
2020-12-27 13:19:42 -0800

    add 2020/12/26 solution

---
+++ 2020/12/26.py
@@ -0,0 +1,74 @@
+"""
+Decode Ways
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+
+Given a non-empty string containing only digits, determine the total number of ways to decode it.
+
+The answer is guaranteed to fit in a 32-bit integer.
+
+ 
+
+Example 1:
+
+Input: s = "12"
+Output: 2
+Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+
+Example 2:
+
+Input: s = "226"
+Output: 3
+Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+Example 3:
+
+Input: s = "0"
+Output: 0
+Explanation: There is no character that is mapped to a number starting with '0'. We cannot ignore a zero when we face it while decoding. So, each '0' should be part of "10" --> 'J' or "20" --> 'T'.
+
+Example 4:
+
+Input: s = "1"
+Output: 1
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 100
+    s contains only digits and may contain leading zero(s).
+"""
+
+# approach: recursion
+# memory: O(n)
+# runtime: O(n)
+import functools
+
+class Solution:
+
+    @functools.lru_cache(maxsize=None)
+    def numDecodings(self, s: str) -> int:
+
+        # base case
+        if len(s) == 0:
+            return 1
+
+        # single check
+        if 0 < int(s[0]) < 10:
+            single = self.numDecodings(s[1:])
+        else:
+            single = 0
+
+        # double check
+        double = 0
+        if len(s) >= 2:
+            if 9 < int(s[0:2]) < 27:
+                double = self.numDecodings(s[2:])
+            
+        return single + double
